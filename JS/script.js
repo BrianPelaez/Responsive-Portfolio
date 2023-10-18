7e51edab8e9c13098228366e2358fc1ba964f9cb
Brian Pelaez <pelaez_brian@hotmail.com>
2023-10-17 22:40:50 -0300

    Change portfolio style

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,12 @@
 /* Typing Animación */
 var typed = new Typed(".typing", {
-    strings:["","Web Designer","Programador","Desarrollador"],
+    strings:["","Web Designer","Full-Stack Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 });
+
+// This adds some nice ellipsis to the description:
+document.querySelectorAll(".projcard-description").forEach(function(box) {
+    $clamp(box, {clamp: 6});
+  });
